79b44973b4852f93ab86e35eb49072ccb3e817dd
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-24 01:40:54 +0800

    Update monitor_pay_status.dart

--- lib/features/panel/xboard/services/monitor_pay_status.dart
+++ lib/features/panel/xboard/services/monitor_pay_status.dart
@@ -30,16 +30,16 @@ class MonitorPayStatus {
           final orderData = orderDetails['data'];
 
           // 检查订单是否被取消
-          if (orderData['status'] is int && orderData['status'] == 2) {
+          if (orderData['data'] is int && orderData['data'] == 2) {
             isPaymentComplete = true; // 停止轮询
             timer.cancel();
             return;
           }
 
           // 检查订单是否已支付
-          if (orderData['status'] is int && orderData['status'] == 0) {
+          if (orderData['data'] is int && orderData['data'] == 0) {
             onPaymentStatusChanged(false); // 通知支付未完成
-          } else if (orderData['status'] is int && orderData['status'] == 3) {
+          } else if (orderData['data'] is int && orderData['data'] == 3) {
             isPaymentComplete = true; // 标记支付完成
             onPaymentStatusChanged(true); // 通知支付完成
             timer.cancel(); // 停止轮询
