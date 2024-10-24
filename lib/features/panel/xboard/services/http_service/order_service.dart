5d27b63fae837f9ae6ac860ca1e2cfad1962f9d5
small-haozi <165977055+small-haozi@users.noreply.github.com>
2024-10-25 05:47:01 +0800

    Update order_service.dart

--- lib/features/panel/xboard/services/http_service/order_service.dart
+++ lib/features/panel/xboard/services/http_service/order_service.dart
@@ -45,7 +45,7 @@ class OrderService {
       String accessToken, int planId, String period) async {
     return await _httpService.postRequest(
       "/api/v1/user/order/save",
-      {"plan_id": planId.toString(), "period": period},
+      {"plan_id": planId.toString(), "period": period.toString()},
       headers: {'Authorization': accessToken},
     );
   }
